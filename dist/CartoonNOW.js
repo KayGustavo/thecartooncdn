ad519a7394a81ab3c06ed4519509b510b1d555bc
SudoReal <46944979+SudoReal@users.noreply.github.com>
2019-05-19 17:39:46 -0300

    Update CartoonNOW.js

--- dist/CartoonNOW.js
+++ dist/CartoonNOW.js
@@ -11,12 +11,16 @@ function getonairLoop(){
       var conteudoTitulo = conteudoSchedule[0].title;
       var conteudoBanner = conteudoSchedule[0].images.banner;
       var conteudoClass = conteudoSchedule[0].ageRating;
+      var conteudoInicio = conteudoSchedule[0].startTime;
+      var conteudoFim = conteudoSchedule[0].endTime;
       if (conteudoClass == "1") conteudoClass = "LIVRE";
       else if (conteudoClass != "1") conteudoClass = conteudoClass + ' ANOS';
       console.log(conteudoTitulo);
       document.getElementById("showBanner").src = conteudoBanner;
       document.getElementById("titulo").innerHTML = 'Título: ' + conteudoTitulo;
       document.getElementById("clas").innerHTML = 'Classificação: ' + conteudoClass;
+      document.getElementById("come").innerHTML = 'Início: ' + new Date(conteudoInicio).toLocaleString('en-GB', { hour:'numeric', minute:'numeric', second:'numeric', hour12:false } );
+      document.getElementById("fim").innerHTML = 'Fim: ' + new Date(conteudoFim).toLocaleString('en-GB', { hour:'numeric', minute:'numeric', second:'numeric', hour12:false } );
     }
   };
   xhttp.open("GET", apiUrl, true);
