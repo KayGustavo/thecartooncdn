f0b88e1d8d01debeef50bfe93f7984aa258833f0
SudoReal <46944979+SudoReal@users.noreply.github.com>
2019-05-19 17:30:46 -0300

    Update CartoonNOW.js

--- dist/CartoonNOW.js
+++ dist/CartoonNOW.js
@@ -12,6 +12,7 @@ function getonairLoop(){
       var conteudoBanner = conteudoSchedule[0].images.banner;
       var conteudoClass = conteudoSchedule[0].ageRating;
       if (conteudoClass == "1") conteudoClass = "LIVRE";
+      else if (conteudoClass != "1") conteudoClass = conteudoClass + ' ANOS';
       console.log(conteudoTitulo);
       document.getElementById("showBanner").src = conteudoBanner;
       document.getElementById("titulo").innerHTML = 'Título: ' + conteudoTitulo;
