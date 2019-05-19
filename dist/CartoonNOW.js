a445966a2dbe4cdc436df0b08fac3ba83c3c7a7c
SudoReal <46944979+SudoReal@users.noreply.github.com>
2019-05-19 16:31:44 -0300

    Update CartoonNOW.js

--- dist/CartoonNOW.js
+++ dist/CartoonNOW.js
@@ -9,7 +9,7 @@ function getonairLoop(){
       var conteudoRoot = conteudo.response[0];
       var conteudoSchedule = conteudoRoot.schedules;
       var conteudoTitulo = conteudoSchedule[0].title;
-      alert(conteudoTitulo);
+      console.log(conteudoTitulo);
       document.getElementById("av").innerHTML = 'teste ' + conteudoTitulo;
     }
   };
