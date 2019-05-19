4713b4f35ed44994d632d14487845776e5fc6895
SudoReal <46944979+SudoReal@users.noreply.github.com>
2019-05-19 15:52:24 -0300

    Update CartoonNOW.js

--- dist/CartoonNOW.js
+++ dist/CartoonNOW.js
@@ -1,16 +1,19 @@
 function getonairLoop(){
+  var apiUrl = 'https://www.nowonline.com.br/avsclient/epg/livechannels/cartoon-network?channel=PCTV&numberOfSchedules=1&includes=images'
   var xhttp = new XMLHttpRequest();
   var sweet = document.getElementsByClassName('sweet')[0];
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      conteudo = JSON.parse(this.responseText);
-      document.getElementById("av").innerHTML = conteudo.flavour + ' - ' + conteudo.flavourName + '. Começou às ' + conteudo.flavourBeg + ', Termina às ' + conteudo.flavourExp + '.';
+      var conteudo = JSON.parse(this.responseText);
+      var conteudoRoot = conteudo.response[0];
+      alert(conteudoRoot);
+      document.getElementById("av").innerHTML = 'teste'
     }
     else {
       document.getElementById("strawberryFlavour").innerHTML = 'Erro! Um problema técnico impossibilita a recepção da programação.';
     }
   };
-  xhttp.open("GET", "https://thecartoonfactory.herokuapp.com/no_ar.php", true);
+  xhttp.open("GET", apiUrl, true);
   xhttp.send();
   setTimeout(getonairLoop, 10000);
 }
