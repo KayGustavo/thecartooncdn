036ca0cff89dccca4cec4515de51ef13e180e409
SudoReal <46944979+SudoReal@users.noreply.github.com>
2019-05-20 11:07:15 -0300

    Update CartoonNOW.js

--- dist/CartoonNOW.js
+++ dist/CartoonNOW.js
@@ -1,34 +1,46 @@
 window.onload = function() {
-function getonairLoop(){
-  var apiUrl = 'https://www.nowonline.com.br/avsclient/epg/livechannels/cartoon-network?channel=PCTV&numberOfSchedules=1&includes=images'
-  var xhttp = new XMLHttpRequest();
-  var sweet = document.getElementsByClassName('sweet')[0];
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var conteudo = JSON.parse(this.responseText);
-      var conteudoRoot = conteudo.response[0];
-      var conteudoSchedule = conteudoRoot.schedules;
-      var conteudoTitulo = conteudoSchedule[0].title;
-      var conteudoBanner = conteudoSchedule[0].images.banner;
-      var conteudoClass = conteudoSchedule[0].ageRating;
-      var conteudoInicio = parseInt(conteudoSchedule[0].startTime * 1000);
-      var conteudoFim = parseInt(conteudoSchedule[0].endTime * 1000);
-      var dateInicio = new Date(conteudoInicio);
-      var dateFim = new Date(conteudoFim);
-      if (conteudoClass == "1") conteudoClass = "LIVRE";
-      else if (conteudoClass != "1") conteudoClass = conteudoClass + ' ANOS';
-      console.log(conteudoTitulo);
-      document.getElementById("showBanner").src = conteudoBanner;
-      document.getElementById("titulo").innerHTML = 'Título: ' + conteudoTitulo;
-      document.getElementById("clas").innerHTML = 'Classificação: ' + conteudoClass;
-      document.getElementById("come").innerHTML = 'Início: ' + dateInicio.toLocaleString('en-GB', { hour:'numeric', minute:'numeric', second:'numeric', hour12:false } );
-      document.getElementById("fim").innerHTML = 'Fim: ' + dateFim.toLocaleString('en-GB', { hour:'numeric', minute:'numeric', second:'numeric', hour12:false } );
+    function getonairLoop() {
+        var apiUrl = 'https://www.nowonline.com.br/avsclient/epg/livechannels/cartoon-network?channel=PCTV&numberOfSchedules=1&includes=images';
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var dateAtual = (new Date).getTime();
+                var conteudo = JSON.parse(this.responseText);
+                var conteudoRoot = conteudo.response[0];
+                var conteudoSchedule = conteudoRoot.schedules;
+                var conteudoTitulo = conteudoSchedule[0].title;
+                var conteudoBanner = conteudoSchedule[0].images.banner;
+                var conteudoClass = conteudoSchedule[0].ageRating;
+                var conteudoInicio = conteudoSchedule[0].startTime * 1000;
+                var conteudoFim = conteudoSchedule[0].endTime * 1000;
+                var dateInicio = new Date(conteudoInicio);
+                var dateFim = new Date(conteudoFim);
+                var dateRestante = dateAtual - conteudoFim;
+                console.log(dateRestante);
+                if (conteudoClass == "1") conteudoClass = "LIVRE";
+                else if (conteudoClass != "1") conteudoClass = conteudoClass + ' ANOS';
+                console.log(conteudoTitulo);
+                document.getElementById("showBanner").src = conteudoBanner;
+                document.getElementById("titulo").innerHTML = 'Título: ' + conteudoTitulo;
+                document.getElementById("clas").innerHTML = 'Classificação: ' + conteudoClass;
+                document.getElementById("come").innerHTML = 'Início: ' + dateInicio.toLocaleString('en-GB', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric',
+                    hour12: false
+                });
+                document.getElementById("fim").innerHTML = 'Fim: ' + dateFim.toLocaleString('en-GB', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric',
+                    hour12: false
+                });
+            }
+        };
+        xhttp.open("GET", apiUrl, true);
+        xhttp.send();
+        setTimeout(getonairLoop, 10000);
     }
-  };
-  xhttp.open("GET", apiUrl, true);
-  xhttp.send();
-  setTimeout(getonairLoop, 10000);
-}
 
-getonairLoop()
+    getonairLoop();
 }
